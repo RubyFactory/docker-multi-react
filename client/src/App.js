d8b003763cdc20dd9fc124905122d091565d4b1f
Evandro Fruhauf <evandro.magalhaes@gmail.com>
2019-06-02 11:16:39 +0100

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Welcome to React Fibonacci page</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">OtherPage</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
